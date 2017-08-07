dba7193734505e156e073a998634b2cf556ada64
DAVID <DAVID@DAVID-PC>
2017-08-06 20:19:46 -0500

    VERSION 27

---
+++ MVC-1/MVC-1/Models/ViewModels/VistaRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_1.Models.ViewModels
+{
+    public class VistaRoles
+    {
+        public string RoleID { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
---
+++ MVC-1/MVC-1/Models/ViewModels/VistaUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_1.Models.ViewModels
+{
+    public class VistaUsuario
+    {
+        public string UsuarioID { get; set; }
+        public string Nombre { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Correo { get; set; }
+
+        public VistaRoles Role { get; set; }
+        public List<VistaRoles> Roles { get; set; }
+
+
+
+    }
+}
